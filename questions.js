6a0fa607f5a972ee8ff608e9a7c815d6c364ca05
ibrahimabdulsalam2010ab-commits <ibrahimabdulsalam2010ab@gmail.com>
2026-07-30 06:32:48 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,48 @@
+const questions = [
+
+{
+question: "What is the capital of Nigeria?",
+options: ["Lagos","Abuja","Kano","Ibadan"],
+answer: "Abuja"
+},
+
+{
+question: "2 + 2 = ?",
+options: ["3","4","5","6"],
+answer: "4"
+},
+
+{
+question: "Who discovered gravity?",
+options: [
+"Albert Einstein",
+"Isaac Newton",
+"Galileo",
+"Nikola Tesla"
+],
+answer: "Isaac Newton"
+},
+
+{
+question: "Which planet is known as the Red Planet?",
+options: [
+"Earth",
+"Mars",
+"Venus",
+"Jupiter"
+],
+answer: "Mars"
+},
+
+{
+question: "HTML stands for?",
+options: [
+"Hyper Text Markup Language",
+"High Text Machine Language",
+"Home Tool Markup Language",
+"Hyper Transfer Markup Language"
+],
+answer: "Hyper Text Markup Language"
+}
+
+];
